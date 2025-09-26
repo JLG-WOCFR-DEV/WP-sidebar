7861c799437b626185513030eb27456eb03e3d2b
Jewoc_DEV <jerome.legousse@gmail.com>
2025-09-26 12:04:53 +0200

    Normalize numeric options during settings revalidation

--- tests/revalidate_opacity_options_test.php
+++ tests/revalidate_opacity_options_test.php
@@ -17,6 +17,7 @@ $GLOBALS['wp_test_options']['sidebar_jlg_settings'] = [
     'mobile_bg_opacity' => 'not-a-number',
     'mobile_blur' => '-15',
     'animation_speed' => '200ms',
+    'social_icon_size' => '90%',
 ];
 
 $repository->revalidateStoredOptions();
@@ -46,6 +47,7 @@ assertSame(
 );
 assertSame(15, $storedAfterRevalidation['mobile_blur'] ?? null, 'Mobile blur uses absolute integer value');
 assertSame(200, $storedAfterRevalidation['animation_speed'] ?? null, 'Animation speed is normalized using absint logic');
+assertSame(90, $storedAfterRevalidation['social_icon_size'] ?? null, 'Social icon size is normalized using absint logic');
 
 if (!$testsPassed) {
     exit(1);
