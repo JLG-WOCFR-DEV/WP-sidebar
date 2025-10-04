6d2d38e93f0930c4bc3661762634a35d5d03e829
Jewoc_DEV <jerome.legousse@gmail.com>
2025-10-04 11:40:15 +0200

    Handle singular taxonomy terms in profile selector

--- sidebar-jlg/src/Frontend/ProfileSelector.php
+++ sidebar-jlg/src/Frontend/ProfileSelector.php
@@ -329,6 +329,14 @@ class ProfileSelector
             $postTypes[$postType] = true;
         }
 
+        $currentObjectId = 0;
+        if (function_exists('get_queried_object_id')) {
+            $queriedId = get_queried_object_id();
+            if (is_int($queriedId) || (is_string($queriedId) && preg_match('/^-?\d+$/', $queriedId) === 1)) {
+                $currentObjectId = absint((int) $queriedId);
+            }
+        }
+
         $queriedObject = null;
         if (function_exists('get_queried_object')) {
             $queriedObject = get_queried_object();
@@ -362,6 +370,98 @@ class ProfileSelector
                     }
 
                     $taxonomies[$taxonomyName] = array_keys($terms);
+                }
+            }
+        }
+
+        if ($currentObjectId === 0 && is_object($queriedObject)) {
+            if (isset($queriedObject->ID) && (is_int($queriedObject->ID) || (is_string($queriedObject->ID) && preg_match('/^-?\d+$/', $queriedObject->ID) === 1))) {
+                $currentObjectId = absint((int) $queriedObject->ID);
+            } elseif (isset($queriedObject->id) && (is_int($queriedObject->id) || (is_string($queriedObject->id) && preg_match('/^-?\d+$/', $queriedObject->id) === 1))) {
+                $currentObjectId = absint((int) $queriedObject->id);
+            }
+        }
+
+        if ($currentObjectId > 0 && $postType !== null && $postType !== '' && function_exists('get_object_taxonomies')) {
+            $objectTaxonomies = get_object_taxonomies($postType);
+            if (is_array($objectTaxonomies)) {
+                foreach ($objectTaxonomies as $objectTaxonomy) {
+                    if (!is_string($objectTaxonomy) && !is_numeric($objectTaxonomy)) {
+                        continue;
+                    }
+
+                    $taxonomyName = $this->sanitizeIdentifier((string) $objectTaxonomy);
+                    if ($taxonomyName === '') {
+                        continue;
+                    }
+
+                    $existingTerms = [];
+                    if (isset($taxonomies[$taxonomyName]) && is_array($taxonomies[$taxonomyName])) {
+                        foreach ($taxonomies[$taxonomyName] as $existingTerm) {
+                            if (!is_string($existingTerm) && !is_numeric($existingTerm)) {
+                                continue;
+                            }
+
+                            $existingTerms[(string) $existingTerm] = true;
+                        }
+                    }
+
+                    $fetchedTerms = null;
+                    if (function_exists('wp_get_post_terms')) {
+                        $fetchedTerms = wp_get_post_terms($currentObjectId, $objectTaxonomy, ['fields' => 'all']);
+                    } elseif (function_exists('get_the_terms')) {
+                        $fetchedTerms = get_the_terms($currentObjectId, $objectTaxonomy);
+                    }
+
+                    if (!is_array($fetchedTerms)) {
+                        continue;
+                    }
+
+                    foreach ($fetchedTerms as $term) {
+                        if (is_object($term)) {
+                            if (isset($term->term_id) && (is_int($term->term_id) || (is_string($term->term_id) && preg_match('/^-?\d+$/', $term->term_id) === 1))) {
+                                $termId = absint((int) $term->term_id);
+                                if ($termId > 0) {
+                                    $existingTerms[(string) $termId] = true;
+                                }
+                            }
+
+                            if (isset($term->slug) && is_string($term->slug)) {
+                                $slug = $this->sanitizeIdentifier($term->slug);
+                                if ($slug !== '') {
+                                    $existingTerms[$slug] = true;
+                                }
+                            }
+                        } elseif (is_array($term)) {
+                            if (isset($term['term_id']) && (is_int($term['term_id']) || (is_string($term['term_id']) && preg_match('/^-?\d+$/', (string) $term['term_id']) === 1))) {
+                                $termId = absint((int) $term['term_id']);
+                                if ($termId > 0) {
+                                    $existingTerms[(string) $termId] = true;
+                                }
+                            }
+
+                            if (isset($term['slug']) && is_string($term['slug'])) {
+                                $slug = $this->sanitizeIdentifier($term['slug']);
+                                if ($slug !== '') {
+                                    $existingTerms[$slug] = true;
+                                }
+                            }
+                        } elseif (is_int($term) || (is_string($term) && preg_match('/^-?\d+$/', $term) === 1)) {
+                            $termId = absint((int) $term);
+                            if ($termId > 0) {
+                                $existingTerms[(string) $termId] = true;
+                            }
+                        } elseif (is_string($term)) {
+                            $slug = $this->sanitizeIdentifier($term);
+                            if ($slug !== '') {
+                                $existingTerms[$slug] = true;
+                            }
+                        }
+                    }
+
+                    if ($existingTerms !== []) {
+                        $taxonomies[$taxonomyName] = array_keys($existingTerms);
+                    }
                 }
             }
         }
