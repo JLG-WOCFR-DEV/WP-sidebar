266d3d7991ebc90f2bad722bad5667385008d799
Jewoc_DEV <jerome.legousse@gmail.com>
2025-10-05 20:47:35 +0200

    Refactor dynamic styles generation and add tests

---
+++ tests/helpers/plugin_helpers.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+use JLG\Sidebar\Cache\MenuCache;
+use JLG\Sidebar\Frontend\ProfileSelector;
+use JLG\Sidebar\Frontend\SidebarRenderer;
+use JLG\Sidebar\Icons\IconLibrary;
+use JLG\Sidebar\Settings\DefaultSettings;
+use JLG\Sidebar\Settings\SettingsRepository;
+
+require_once __DIR__ . '/../../sidebar-jlg/sidebar-jlg.php';
+
+function sidebar_jlg_create_test_sidebar_renderer(): SidebarRenderer
+{
+    $pluginFile = __DIR__ . '/../../sidebar-jlg/sidebar-jlg.php';
+    $version = 'test';
+
+    $defaults = new DefaultSettings();
+    $icons = new IconLibrary($pluginFile);
+    $settings = new SettingsRepository($defaults, $icons);
+    $cache = new MenuCache();
+    $profileSelector = new ProfileSelector($settings);
+
+    return new SidebarRenderer($settings, $icons, $cache, $profileSelector, $pluginFile, $version);
+}
